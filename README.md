68223ea2009205e848e605b72d2900393e5a5b69
gongshunkai <49078111@qq.com>
2018-01-02 15:35:54 +0800

    list-row-group

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
 # list-row-group
 angular组件 - 模拟两个多选select(multiple左右)添加、删除选项和取值
+
+## 配置
+在html头部引用：  
+```
+    list-row-group.css
+    list-row-group.min.js
+```
+## 使用
+list-row-group的属性和方法：
+value 映射数据源id字段名
+text 映射数据源文本字段名
+template-url 模板地址
+add-item 添加项目的回调方法，该方法有1个参数，包含：待添加项的数据集合，如需处理异步请求，请返回一个promise对象。
+drop-item 删除项目的回调方法，该方法有1个参数，包含：待删除项的id数组，如需处理异步请求，请返回一个promise对象。
+
+list-group-source的属性和方法：
+header 列表的标题
+source 列表的数据源
+multiple 是否多选
+template-url 模板地址
+selected 选中项目的回调方法，该方法有3个参数，包含：选中项数据、选中项索引值、选中项是否激活
+
+list-group-target的属性和方法：
+header 列表的标题
+source 列表的数据源
+multiple 是否多选
+template-url 模板地址
+selected 选中项目的回调方法，该方法有3个参数，包含：选中项数据、选中项索引值、选中项是否激活
+
+```html
+<list-row-group value="id" text="name" add-item="addItem(items)" drop-item="dropItem(values)">
+	<list-group-source header="标题" source="sourcelist" selected="selected(item,index,active)"></list-group-source>
+	<form-group-extra>
+		...
+	</form-group-extra>
+	<list-group-target header="标题" source="targetlist" multiple="true"></list-group-target>
+</list-row-group>
+
+```
+
+```javascript
+var app = angular.module('myApp', ['listRowGroup']);
+app.controller('myCtrl', ['$scope','$http','$q',function($scope,$http,$q) {
+
+	$scope.sourcelist = [];
+	$scope.targetlist = [];
+	
+	$scope.selected = function(item,index,active){
+		....
+	};
+
+	$scope.addItem = function(items){
+		....
+	};
+
+	$scope.dropItem = function(values){
+
+		//如需处理异步请求，请返回一个promise对象
+		//这样做的好处是数据库删除成功才会删除列表中的选中项（添加/删除列表中的数据由组件完成，用户只需关心与后端的交互），否则处理错误的回调方法。 
+
+		var deferred = $q.defer();
+		
+		//向后端请求删除数据
+		$http.post('...',data)
+		.success(function(data){
+			alert('删除的id: ' + values);
+			deferred.resolve(data);
+		})
+		.error(function(msg){
+			deferred.reject(msg);
+		});
+
+		return deferred.promise;
+	};
+
+}]);
+```
---
+++ scripts/list-row-group.js
@@ -0,0 +1,268 @@
+(function(window, angular) {
+
+    'use strict';
+
+    var myPromise = {
+        then: (function (data) {
+            return function (callback) {
+                callback.call(window, data);
+                return myPromise;
+            };
+        })(myPromise),
+        catch: (function (data) {
+            return function (callback) {
+                callback.call(window, data);
+                return myPromise;
+            };
+        })(myPromise),
+        finally: (function (data) {
+            return function (callback) {
+                callback.call(window, data);
+                return myPromise;
+            };
+        })(myPromise)
+    };
+
+    var listGroup = {
+
+        restrict:'AE',  
+        require: '^listRowGroup',
+        templateUrl: function(element, attrs) {
+            return attrs.templateUrl || 'list-group-item.html';
+        },
+        replace:true,
+        scope:{
+            header:'@',
+            source:'=',
+            multiple:'=',
+            selected:'&'
+        },
+        link:function(scope, element, attrs, listRowGroupCtrl){
+
+            scope.text = listRowGroupCtrl.getText();
+
+            scope.$selected = function(i){
+
+                if(scope.multiple){
+                    if(scope.active[i]){
+                        scope.active[i] = false;
+                    }else{
+                        scope.active[i] = true;
+                    }
+                }else{    
+                    angular.forEach(scope.source,function(item,index){
+                        scope.active[index] = false;
+                    });
+
+                    scope.active[i] = true;
+                }   
+
+                scope.selected({item:scope.source[i],index:i,active:scope.active[i]});
+            };
+        }
+    };
+
+    angular.module('listRowGroup', [])
+
+    .directive('listRowGroup', function() {
+
+        return {
+            restrict:'AE',  
+            transclude:{
+                'source':'listGroupSource',
+                'extra':'formGroupExtra',
+                'target':'listGroupTarget'
+            },  
+            templateUrl: function(element, attrs) {
+                return attrs.templateUrl || 'list-row-group.html';
+            },
+            replace:true,
+            scope:{
+                text:'@',
+                value:'@',
+                addItem:'&',
+                dropItem:'&'
+            },
+            controller:['$scope',function($scope){
+
+                var source = {};
+
+                var target = {};
+
+                this.getText = function(){
+                    return $scope.text;
+                };
+
+                this.setSource = function(key,value){
+                    source[key] = value;
+                };
+
+                this.setTarget = function(key,value){
+                    target[key] = value;
+                };
+
+                this.setDoubleAngle = function(type,mode){
+                    $scope['showDoubleAngle' + type] = mode;
+                };
+
+                $scope.$addItem = function(type){
+                    
+                    var cache = {};
+                    var items = [];
+
+                    angular.forEach(target.data,function(item){
+                        var id = item[$scope.value];
+                        cache[id] = true;
+                    });
+
+                    angular.forEach(source.data,function(item,index){
+
+                        var id = item[$scope.value];
+                            item = angular.copy(item);
+
+                        if((function(){
+                            if(type == 'all'){
+                                if(!cache[id]){
+                                    return true;
+                                }
+                            }else{
+                                if(source.act[index] && !cache[id]){
+                                    return true;
+                                }
+                            }
+                        })()){        
+                            items.push(item);
+                        }
+                    });
+
+                    $scope.loading = true;
+
+                    var promise = $scope.addItem({items:items}) || {};
+                    
+                    (promise = promise.then ? promise : angular.extend({},myPromise))
+                    .then(function (data) {
+                        angular.forEach(items,function(item){
+                            target.data.push(item);
+                        });
+                    })
+                    .catch(function(data){
+                        //待实现
+                    })
+                    .finally(function(data){
+                        $scope.loading = false;
+                    });
+                };
+
+                $scope.$dropItem = function(type){
+
+                    var values = [];
+                    var indexes = [];
+
+                    angular.forEach(target.data,function(item,index){
+                        if(type == 'all' || target.act[index]){
+                            values.push(target.data[index][$scope.value]);
+                            indexes.unshift(index);
+                        }
+                    });
+
+                    $scope.loading = true;
+
+                    var promise = $scope.dropItem({values:values}) || {};
+                    
+                    (promise = promise.then ? promise : angular.extend({},myPromise))
+                    .then(function (data) {
+
+                       angular.forEach(indexes,function(index){
+                            target.data.splice(index,1);
+                            target.act[index] = false;
+                        });
+
+                    })
+                    .catch(function(data){
+                        //待实现
+                    })
+                    .finally(function(data){
+                        $scope.loading = false;
+                    });
+                };
+            }]
+        }
+    })
+
+    .directive('listGroupSource',function(){
+
+        return angular.extend({},listGroup,{
+
+            link:function(scope, element, attrs, listRowGroupCtrl){
+
+                listRowGroupCtrl.setSource('act',scope.active = []);
+                listRowGroupCtrl.setDoubleAngle('Right',scope.multiple);
+
+                scope.$watch('source',function(){       
+                    angular.forEach(scope.source,function(item,index){
+                        scope.active[index] = false;
+                    });
+                    listRowGroupCtrl.setSource('data',scope.source);  
+                });
+
+                listGroup.link(scope, element, attrs, listRowGroupCtrl);
+            }
+        });
+
+    })
+
+    .directive('listGroupTarget',function(){
+
+        return angular.extend({},listGroup,{
+            
+            link:function(scope, element, attrs, listRowGroupCtrl){
+
+                listRowGroupCtrl.setTarget('act',scope.active = []);
+                listRowGroupCtrl.setDoubleAngle('Left',scope.multiple);
+                
+                scope.$watch('source',function(){
+                    listRowGroupCtrl.setTarget('data',scope.source);
+                });
+
+                listGroup.link(scope, element, attrs, listRowGroupCtrl);
+            }
+        });
+
+    })
+
+    .run(["$templateCache", function($templateCache) {
+        $templateCache.put("list-row-group.html",
+        "<div class=\"list-row-group\">\n" +
+        "  <div class=\"col-xs-5\">\n" +
+        "      <div ng-transclude=\"source\"></div>\n" +
+        "  </div>\n" +
+        "  <div class=\"col-xs-2\">\n" +
+        "    <div ng-transclude=\"extra\"></div>\n" +
+        "    <div class=\"form-group\">\n" +
+        "      <div class=\"btn-group-vertical btn-block\" role=\"group\">\n" +
+        "        <button type=\"button\" class=\"btn btn-primary\" ng-if=\"showDoubleAngleRight\" ng-click=\"$addItem('all')\" ng-disabled=\"loading\"><i class=\"icon-double-angle-right\"></i></button>\n" +
+        "        <button type=\"button\" class=\"btn btn-info\" ng-click=\"$addItem()\" ng-disabled=\"loading\"><i class=\"icon-angle-right\"></i></button>\n" +
+        "        <button type=\"button\" class=\"btn btn-info\" ng-click=\"$dropItem()\" ng-disabled=\"loading\"><i class=\"icon-angle-left\"></i></button>\n" +
+        "        <button type=\"button\" class=\"btn btn-primary\" ng-if=\"showDoubleAngleLeft\" ng-click=\"$dropItem('all')\" ng-disabled=\"loading\"><i class=\"icon-double-angle-left\"></i></button>\n" +
+        "      </div>\n" +
+        "    </div>\n" +
+        "  </div>\n" +
+        "  <div class=\"col-xs-5\">\n" +
+        "      <div ng-transclude=\"target\"></div>\n" +
+        "  </div>\n" +
+        "</div>\n" +
+        "");
+    }])
+
+    .run(["$templateCache", function($templateCache) {
+        $templateCache.put("list-group-item.html",
+        "    <div class=\"list-group-wrapper\">\n" +
+        "      <div class=\"list-group-header\">{{header}}</div>\n" +
+        "        <div class=\"list-group\">\n" +
+        "          <a href=\"\" class=\"list-group-item\" ng-class=\"{'list-group-item-warning':active[$index]}\" ng-click=\"$selected($index)\" ng-repeat=\"item in source\">{{item[text]}}</a>\n" +
+        "      </div>\n" +
+        "    </div>\n" +
+        "");
+    }]);
+
+})(window, window.angular);
